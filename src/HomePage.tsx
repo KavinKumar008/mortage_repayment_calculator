fe6d4b04e261e141df860f3fa5d43e6a9d27f17c
KavinKumar008 <kavikumarsraja@gmail.com>
2025-01-11 22:36:33 +0530

    styling works going

---
+++ src/HomePage.tsx
@@ -0,0 +1,96 @@
+import { FaPoundSign } from "react-icons/fa";
+import calculator from "./assets/icon-calculator.jpg";
+import illustration from "./assets/illustration-empty.jpg";
+
+const HomePage = () => {
+  return (
+    <main className="w-[1000px] h-[500px] flex shadow-2xl rounded-3xl">
+      <section className="w-[50%] h-auto border-2 border-yellow-400 p-8">
+        <div className="flex justify-between items-center">
+          <p className="text-[1.6rem] font-bold">Mortgage Calculator</p>
+          <p className="border-b border-gray-400 cursor-pointer">Clear All</p>
+        </div>
+        <div className="relative mt-4">
+          <p className="text-gray-500">Mortgage Amount</p>
+          <div className="absolute h-[48px] w-[50px] flex justify-center items-center bg-blue-100 rounded-lg">
+            <FaPoundSign className="text-gray-500 font-light" />
+          </div>
+          <input
+            type="text"
+            className="w-full border border-gray-500 p-3 rounded-md outline-none"
+          />
+        </div>
+        <div className="flex gap-8 mt-4">
+          <div className="relative">
+            <p className="text-gray-500">Mortgage Term</p>
+            <input
+              type="text"
+              className="border border-gray-500 p-3 rounded-md mt-2 outline-none w-full"
+            />
+            <div className="absolute right-3 top-[50%] translate-y-[-50%] text-gray-500">
+              <span>years</span>
+            </div>
+          </div>
+          <div>
+            <p className="text-gray-500">Interest Rate</p>
+            <input
+              type="text"
+              name=""
+              id=""
+              className="border border-gray-500 p-3 rounded-md mt-2 outline-none"
+            />
+            <span>%</span>
+          </div>
+        </div>
+        <div className="mt-4">
+          <p className="text-gray-500">Mortgage Type</p>
+          <div className="flex flex-col gap-3">
+            <label
+              htmlFor=""
+              className="w-full border border-gray-500 p-3 rounded-md flex gap-2 items-center  cursor-pointer"
+            >
+              <input type="radio" />
+              <span className="text-blue-950 font-bold">Repayment</span>
+            </label>
+            <label
+              htmlFor=""
+              className="w-full border border-gray-500 p-3 rounded-md flex gap-2 items-center cursor-pointer"
+            >
+              <input type="radio" />
+              <span className="text-blue-950 font-bold">Interest Only</span>
+            </label>
+          </div>
+        </div>
+        <div className="">
+          <button
+            type="button"
+            className="flex gap-3 items-center mt-5 p-3 px-10 bg-yellow-300 rounded-3xl"
+          >
+            <img src={calculator} alt="calc" className="h-4 bg-transparent" />
+            <span className="text-blue-950 font-bold">
+              Calculate Repayments
+            </span>
+          </button>
+        </div>
+      </section>
+      <section className="w-[50%] h-auto border-2 border-green-600">
+        <div>
+          <img
+            src={illustration}
+            alt="illustration"
+            className="h-[200px] bg-transparent"
+          />
+        </div>
+        <div>
+          <h3>Results shown here</h3>
+          <p>
+            Complete the form and click "calculate repayments" to see what your
+            monthly repayments would be.
+          </p>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default HomePage;
